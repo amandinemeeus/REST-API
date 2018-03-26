3102b766ddb65382ed42b4cc0830f1978076b4dd
amandinemeeus <amandine.meeus@alumni.ulb.be>
2018-03-26 14:46:20 +0200

    envoi

---
+++ src/mongo.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+import { userSchema } from './schemas'
+
+const url = 'mongodb://localhost:27017/local'
+const options = {
+    promiseLibrary: Promise,
+    
+}
+
+mongoose.connect(url, options)
+
+mongoose.connection.on('connected', () =>
+    console.log('[MongoDB] is running on port 27017')
+)
+
+mongoose.connection.on('disconnected', () =>
+    console.warn('[MongoDB] is not connected')
+)
+
+mongoose.model('User', userSchema)
+
+
